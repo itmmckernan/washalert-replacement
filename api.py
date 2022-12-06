dff76bafc16bfccc3ec0bba2980c4eae065d8dee
ian <git@itmm.tk>
2022-12-06 03:00:47 -0800

    Inital commit

---
+++ api.py
@@ -0,0 +1,32 @@
+import requests
+import json
+import configparser
+
+config = configparser.ConfigParser()
+config.read('secrets.ini')
+
+headers={"X-API-KEY": config['Main']['api_key']}
+baseURL = "https://getwashconnect.com/api/rooms/room_status"
+token = config['Main']['token']
+location_id = "13046"
+rooms = {'Yosemite - Tower 0': '1', 'Yosemite - Tower 2': '2', 'Yosemite - Tower 4': '3', 'Yosemite - Tower 5': '4', 'Yosemite - Tower 7': '5', 'Yosemite - Tower 9': '6', 'Sierra Madre - Tower 0': '7', 'Sierra Madre - Tower 1': '8', 'Sierra Madre - Tower 2': '9', 'Sierra Madre - Tower 3': '10', 'Sierra Madre - Tower 4': '11', 'Sierra Madre - Tower 5': '12', 'Tenaya - North Mtn': '13', 'Tenaya - South Mtn': '14', 'Sequoia - North Mtn': '15', 'Sequoia - South Mtn': '16', 'Muir - North Mtn': '17', 'Muir - South Mtn': '18', 'Santa Lucia - North Mtn': '19', 'Santa Lucia - South Mtn': '20', 'Trinity - North Mtn': '21', 'Trinity - South Mtn': '22', 'Whitney - North Mtn': '23', 'Palomar - North Mtn': '24', 'Diablo - North Mtn': '25', 'Cerro Vista - Morro': '26', 'Cerro Vista - Hollister': '27', 'Cerro Vista - Bishop': '28', 'Poly Canyon Village - Aliso': '29', 'Poly Canyon Village - Corralitos': '30', 'Poly Canyon Village - Dover': '31', 'Poly Canyon Village - Estrella': '32', 'Poly Canyon Village - Foxen': '33', 'Poly Canyon Village - Gypsum': '34', 'Poly Canyon Village - Huasna': '35', 'Poly Canyon Village - Inyo': '36', 'Yakitutu - Bldg A, A1-37': '37', 'Yakitutu - Bldg B, B1': '38', 'Yakitutu - Bldg C, C1-37': '39', 'Yakitutu - Bldg D, D1-39': '40', 'Yakitutu - Bldg E, E1-39': '41', 'Yakitutu - Bldg F, F1-37': '42', 'Yakitutu - Bldg G, G1-39': '43', 'Freemont Bldg 109': '44'}
+
+room_groups = {
+    "Cerro Vista": ["26", "27", "28"],
+    "North Mountain": ["23", "24", "25"],
+    "Poly Canyon Village": ["29", "30", "31", "32", "33", "34", "35", "36"],
+    "Yosemite": ['1', '2', '3', '4', '5', '6'],
+    "Sierra Madre": ["7", '8','9','10','11', '12']
+}
+
+def url_asm(baseurl, location_id, room_num, token):
+    return f'{baseurl}?location_id={location_id}&room_number={room_num}&token={token}'
+
+def get_room(room_num):
+    return json.loads(requests.get(url_asm(baseURL, location_id, room_num, token), headers=headers).content)
+
+def get_rooms(rooms=rooms):
+    out_dict = {}
+    for room_name, room_num in rooms.items():
+        out_dict[room_name] = get_room(room_num)
+    return out_dict
---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, render_template
+from markupsafe import escape
+import api
+app = Flask(__name__)
+
+@app.route('/')
+def directory():
+    return render_template('directory.html', room_groups=api.room_groups.keys())
+
+@app.route('/all')
+def overview_alls():
+    rooms = api.get_rooms()
+    return render_template('room.html', api_info=rooms, title="Room Overview")
+
+@app.route('/byGroup/<string:room_group>')
+def roomGroup(room_group):
+    if room_group in api.room_groups.keys():
+        room_dict = {}
+        for room in api.room_groups[room_group]:
+            room_dict[list(api.rooms.keys())[list(api.rooms.values()).index(room)]] = api.get_room(room)
+        return render_template('room.html', api_info=room_dict, title=room_group)
+    else:
+        return "<h1>Error: Invalid room</h1>"
+
+@app.route('/byName/<string:room_name>')
+def singleRoom(room_name):
+    if room_name in api.rooms.keys():
+        room_info = api.get_room(api.rooms[room_name])
+        return render_template('room.html', api_info={room_name: room_info}, title=room_name)
+    else:
+        return "<h1>Error: Invalid room</h1>"
+
+if __name__ == '__main__':
+   app.run(debug=False, port=69420, host='0.0.0.0')
